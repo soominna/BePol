06bae80c91902b909f2f4baf8d2d5836ba3be8c6
dltmddus1998 <dltmddus1998@naver.com>
2022-07-26 12:31:28 +0900

    Setting: 백 초기 환경 설정

---
+++ server/app.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import dotenv from 'dotenv'
+import cors from 'cors';
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/", (req, res, next) => {
+    res.send("API Server!!");
+});
+
+app.use((req, res, next) => {
+    res.sendStatus(404);
+});
+
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.sendStatus(500);
+});
+
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {
+    console.log(`SERVER Started on ${port} port`);
+});
