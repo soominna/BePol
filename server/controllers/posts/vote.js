79d1989ed886a2eacbf3df195b78b89c110ee54a
dltmddus1998 <dltmddus1998@naver.com>
2022-08-01 00:17:05 +0900

    Feat: 찬반투표 & 찬반투표 취소 API 기능(트랜잭션 처리 완료)

--- server/controllers/posts/vote.js
+++ server/controllers/posts/vote.js
@@ -1,93 +1,82 @@
 import * as postAnswerRepository from "../../models/postAnswer.js";
 
 export const voteToPost = async (req, res, next) => {
-    /**
-     * 기능: 발의문 찬반투표 & 투표 취소 기능
-     * 작성자: 이승연
-     * 📌 투표 기능 ✔︎
-     * 📌 게시물 agrees, disagrees 반영 ✔︎
-     * 📌 예외 처리 - unauthorized user, 이미 투표한 사람 ✔︎
-     * 📌 로그인 적용 ❌ (소셜로그인 부분 merge 후 진행할 계획!)
-     */
+  /**
+   * 기능: 발의문 찬반투표 & 투표 취소 기능
+   * 작성자: 이승연
+   * 📌 투표 기능 ✔︎
+   * 📌 게시물 agrees, disagrees 반영 ✔︎
+   * 📌 예외 처리 - unauthorized user, 이미 투표한 사람 ✔︎
+   * 📌 로그인 적용 ❌ (소셜로그인 부분 merge 후 진행할 계획!)
+   * 📌 트랜잭션 처리
+   */
 
-    /**
-     * agree === false -> 반대
-     * agree === true -> 찬성
-     */
-    const { agree } = req.body;
-    const { accesstoken } = req.headers;
-    const { postId } = req.params;
-    const userId = "62e1eb6f6cc8d5e6d3bfac2d"; // 소셜로그인 구현되면 변경
+  /**
+   * agree === false -> 반대
+   * agree === true -> 찬성
+   */
+  const { agree } = req.body;
+  const { accesstoken } = req.headers;
+  const { postId } = req.params;
+  const userId = "62e1eb6f6cc8d5e6d3bfac2d"; // 소셜로그인 구현되면 변경
+  const votedUser = await postAnswerRepository.getUserIdAnswered(userId);
 
-    if (!accesstoken) { // user 정보 불일치시 error
-        res.status(401).json({
-            message: "Unauthorized user",
-        });
-    }
+  if (!accesstoken) {
+    // user 정보 불일치시 error
+    return res.status(401).json({
+      message: "Unauthorized user",
+    });
+  } else if (votedUser) {
+    // 이미 투표한 경우
+    return res.status(403).json({
+      message: "Already voted user!",
+    });
+  } else {
+    postAnswerRepository.addAnswerTransaction(postId, userId, agree);
 
-    const votedUser = await postAnswerRepository.getUserIdAnswered(userId);
-
-    if (votedUser) { // 이미 투표한 경우
-        return res.status(403).json({
-            message: "Already voted user!",
-        });
-    }
-
-    const data = await postAnswerRepository.addAnswer(postId, userId, agree);
-
-    if (data) {
-        // post collection - agrees, disagrees 적용
-        await (
-            agree === true ?
-            postAnswerRepository.addAgrees(postId) :
-            postAnswerRepository.addDisagrees(postId)
-        );
-
-        return res.status(201).json({
-            message: "Voted successfully",
-            "agree": data.answer,
-        });
-    }
-}
+    return res.status(201).json({
+      message: "Voted successfully",
+    });
+  }
+};
 
 export const voteDeleteToPost = async (req, res, next) => {
-    /**
-     * 기능: 찬반투표 취소
-     * 작성자: 이승연
-     * 📌 투표 취소 기능 ✔︎
-     * 📌 게시물 agrees, disagrees 반영 ✔︎
-     * 📌 예외 처리 - unauthorized user, postAnser 컬렉션에 해당 유저가 없는 사람 (투표 안함) ✔︎
-     * 📌 로그인 적용 ❌ (소셜로그인 부분 merge 후 진행할 계획!)
-     */
+  /**
+   * 기능: 찬반투표 취소
+   * 작성자: 이승연
+   * 📌 투표 취소 기능 ✔︎
+   * 📌 게시물 agrees, disagrees 반영 ✔︎
+   * 📌 예외 처리 - unauthorized user, postAnser 컬렉션에 해당 유저가 없는 사람 (투표 안함) ✔︎
+   * 📌 로그인 적용 ❌ (소셜로그인 부분 merge 후 진행할 계획!)
+   */
 
-    const { agree } = req.body;
-    const { accesstoken } = req.headers;
-    const { postId } = req.params;
-    const userId = "62e1eb6f6cc8d5e6d3bfac2d"; // 소셜로그인 구현되면 변경
+  /**
+   * 해당 유저의 투표 기록을 취소
+   * 1. postAnswer에서 해당 유저아이디에 일치하는 answer값 가져오기
+   * 2. 이 answer값을 지우는것
+   */
+  const { accesstoken } = req.headers;
+  const { postId } = req.params;
+  const userId = "62e1eb6f6cc8d5e6d3bfac2d"; // 소셜로그인 구현되면 변경
+  const userPostAnswer = await postAnswerRepository.findUserAnswer(userId);
+  const { answer } = userPostAnswer;
+  const votedUser = await postAnswerRepository.getUserIdAnswered(userId);
 
-    if (!accesstoken) { // user 정보 불일치시 error
-        res.status(401).json({
-            message: "Unauthorized user",
-        });
-    }
-
-    const votedUser = await postAnswerRepository.getUserIdAnswered(userId);
-
-    if (!votedUser) { // 투표 안한 경우
-        return res.status(403).json({
-            message: "No vote record of this user!!",
-        });
-    }
-
-    await postAnswerRepository.deleteAnswer(postId, userId);
-
-    await (
-        agree === true ?
-        postAnswerRepository.substractAgrees(postId) :
-        postAnswerRepository.substractDisagrees(postId)
-    );
+  if (!accesstoken) {
+    // user 정보 불일치시 error
+    res.status(401).json({
+      message: "Unauthorized user",
+    });
+  } else if (!votedUser) {
+    // 투표 안한 경우
+    return res.status(403).json({
+      message: "No vote record of this user!!",
+    });
+  } else {
+    postAnswerRepository.deleteAnswerTransaction(postId, userId, answer);
 
     return res.status(200).json({
-        message: "Vote is deleted!!",
-    })
-}
+      message: "Vote is deleted!!",
+    });
+  }
+};
--- server/models/postAnswer.js
+++ server/models/postAnswer.js
@@ -1,6 +1,8 @@
 "use strict";
 import mongoose from "mongoose";
+import pkg from "mongoose";
 import Post from "./post.js";
+const { startSession } = pkg;
 
 const newSchema = mongoose.Schema({
   id: {
@@ -25,54 +27,91 @@ const Post_answers = mongoose.model("Post_answers", newSchema);
 
 export const getUserIdAnswered = async (userId) => {
   return Post_answers.findOne({ userId });
-}
+};
 
 export const addAnswer = async (postId, userId, agree) => {
-  /**
-   * 기능: Post_answer collection에 
-   */
   return Post_answers.create({
     id: postId + userId,
     postId,
     userId,
-    answer: agree
+    answer: agree,
   });
-}
+};
 
 export const addAgrees = async (postId) => {
-  return Post.findOne({ id: postId }, { userId: false })
-    .then(post => {
-      post.agrees++;
-      return post.save();
-    });
-}
+  return Post.findOne({ id: postId }, { userId: false }).then((post) => {
+    post.agrees++;
+    return post.save();
+  });
+};
 
 export const addDisagrees = async (postId) => {
-  return Post.findOne({ id: postId }, { userId: false })
-    .then(post => {
-      post.disagrees++;
-      return post.save();
-    })
-}
+  return Post.findOne({ id: postId }, { userId: false }).then((post) => {
+    post.disagrees++;
+    return post.save();
+  });
+};
+
+export const addAnswerTransaction = async (postId, userId, agree) => {
+  /**
+   * 투표 반영 -> Post, Post_answer
+   */
+  const session = await startSession();
+  try {
+    session.startTransaction();
+
+    await addAnswer(postId, userId, agree);
+    await (agree === true ? addAgrees(postId) : addDisagrees(postId));
+
+    await session.commitTransaction();
+    session.endSession();
+  } catch (err) {
+    await session.abortTransaction();
+    session.endSession();
+    console.error(err);
+  }
+};
+
+export const findUserAnswer = async (userId) => {
+  return Post_answers.findOne({ userId });
+};
 
 export const deleteAnswer = async (postId, userId) => {
-  return Post_answers.deleteOne({ id: postId + userId })
-}
+  return Post_answers.deleteOne({ id: postId + userId });
+};
 
 export const substractAgrees = async (postId) => {
-  return Post.findOne({ id: postId }, { userId: false })
-    .then(post => {
-      post.agrees--;
-      return post.save();
-    });
-}
+  return Post.findOne({ id: postId }, { userId: false }).then((post) => {
+    post.agrees--;
+    return post.save();
+  });
+};
 
 export const substractDisagrees = async (postId) => {
-  return Post.findOne({ id: postId }, { userId: false })
-    .then(post => {
-      post.disagrees--;
-      return post.save();
-    });
-}
+  return Post.findOne({ id: postId }, { userId: false }).then((post) => {
+    post.disagrees--;
+    return post.save();
+  });
+};
+
+export const deleteAnswerTransaction = async (postId, userId, answer) => {
+  /**
+   * 투표 취소 반영 -> Post, Post_answer
+   */
+  const session = await startSession();
+  try {
+    session.startTransaction();
+
+    await deleteAnswer(postId, userId);
+    await (answer === true ? substractAgrees(postId) : substractDisagrees(postId));
+
+    await session.commitTransaction();
+    session.endSession();
+  } catch (err) {
+    await session.abortTransaction();
+    session.endSession();
+    console.error(err);
+  }
+};
 
 export default Post_answers;
