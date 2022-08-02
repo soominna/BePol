c8f61f6620689f433757c5f37440fea2ac489963
Hojewl <shyewon1203@gmail.com>
2022-08-02 09:37:17 +0900

    Feat: 카카오 로그인 연결

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export default function Login() {
+  const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.REACT_APP_KAKAO_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_KAKAO_REDIRECT}&response_type=code`;
   return <div>Login</div>;
 }
