cc474b97e123eb9fcce0e0440aebfd6a509a635e
Soomin Na <soominnna@gmail.com>
2022-08-03 00:52:42 +0900

    style: 명확성을 위해 응답 코드 send에서 json으로 수정

--- server/controllers/comments/likes.js
+++ server/controllers/comments/likes.js
@@ -1,4 +1,4 @@
-import * as commentLikeRepository from "../../service/commentLike.js";
+import * as commentLikeRepository from "../../Services/commentLike.js";
 
 export const postLike = async (req, res) => {
   /**
@@ -22,9 +22,9 @@ export const postLike = async (req, res) => {
     );
 
     if (updatedComment)
-      res.status(201).send({ likesCount: updatedComment.likes });
+      res.status(201).json({ likesCount: updatedComment.likes });
     else res.sendStatus(500);
-  } else res.status(409).send({ message: "You already liked the comment" });
+  } else res.status(409).json({ message: "You already liked the comment" });
 };
 
 export const deleteLike = async (req, res) => {
