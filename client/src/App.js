1623e611dbc50e30a67b043f863b53841ee9091b
Hojewl <shyewon1203@gmail.com>
2022-08-04 07:13:59 +0900

    Chore: App에서 dispatch 삭제

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 // import Detail from "./pages/Detail.jsx";
 import Login from "./pages/Login.jsx";
 // import Main from "./pages/Main.jsx";
@@ -7,13 +7,10 @@ import Write from "./pages/Write.jsx";
 import Header from "./components/Header.jsx";
 import Modal from "./components/Modal.jsx";
 import LoginModal from "./components/LoginModal.jsx";
-import { showLoginModal } from "./reducers/modalSlice";
 
 function App() {
-  const dispatch = useDispatch();
   const isLogin = useSelector((state) => state.login.isLogin);
   const { loginModal, staticModal } = useSelector((state) => state.modal);
-  dispatch(showLoginModal(true));
 
   return (
     <BrowserRouter>
